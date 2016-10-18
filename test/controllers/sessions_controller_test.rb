b15acfc1412ddbaf97ecc8030d92adb24018cac5
Yasmin <yasminor@gmail.com>
2016-10-18 15:52:32 -0700

    WIP: starting tests for sessions controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should redirect to login failure path if the merchant isn't found" do
+    # if auth_has['uid'].nil?
+    #   assert_redirected_to { controller: 'sessions' action: 'create' }
+    # end
+  end
 end
