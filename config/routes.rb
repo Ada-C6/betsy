52ae8bb98829ae2cdc2795bd510623e1feddc35e
Yasmin <yasminor@gmail.com>
2016-10-18 15:51:18 -0700

    Made some changes to get rid of unnecessary ones

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-  resources :merchants, except: [:index]
+  resources :merchants, only: [:show]
 
   resources :reviews, except: [:index, :show]
 
