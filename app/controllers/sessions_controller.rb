f0b4ff894f6236818bc8de9cb5d208c87e0de8d5
Yasmin <yasminor@gmail.com>
2016-10-18 15:51:49 -0700

    Added a create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,22 @@
 class SessionsController < ApplicationController
-  
+
+
+  def create # TODO: Need to add a 'uid' column to the Merchant db in order to create a session for a merchant! Look at line 8
+    auth_hash = request.env['omniauth.auth']
+    redirect_to login_failure_path if auth_hash['uid'].nil?
+
+    @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github') # Remember: .find_by always returns one object (the first that matches)
+    if @merchant.nil?
+      # merchant doesn't match anything in the DB.
+      # Attempt to create a new merchant
+      @merchant = Merchant.build_from_github(auth_hash)
+      render :creation_failure unless @merchant.save # This line saves the newly built @merchant to the database if it can be saved
+    end
+    # Save the user ID in the session
+    session[:user_id] = @merchant.id
+
+    redirect_to sessions_path
+  end
+
+
 end
