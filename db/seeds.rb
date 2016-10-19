3215e9472008e44d842026f99bd4460370f05875
Shari Meggs <smeggs1@gmail.com>
2016-10-19 15:48:44 -0700

    Changed the index page for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,42 +12,42 @@ product = [
     description: "a nice pair of slacks with an elastic waist",
     stock: 15,
     price: 850,
-    photo_url: "https://s-media-cache-ak0.pinimg.com/236x/72/77/86/7277863e594b27677d2228101ef7e293.jpg/200/300"
+    photo_url: "https://s-media-cache-ak0.pinimg.com/236x/72/77/86/7277863e594b27677d2228101ef7e293.jpg"
   },
   {
     name: "llama scarf",
     description: "long and warm and not made with other llamas",
     stock: 20,
     price: 1000,
-    photo_url: "http://www.yesandyes.org/wp-content/uploads/2011/01/scarf5.jpg/200/300"
+    photo_url: "http://www.yesandyes.org/wp-content/uploads/2011/01/scarf5.jpg"
   },
   {
     name: "penguin hawaiin shirt",
     description: "For the penguin who wants to get away",
     stock: 4,
     price: 2000,
-    photo_url: "https://s-media-cache-ak0.pinimg.com/originals/bf/8a/c8/bf8ac87e3ae2ff56ec7db987edd60d7d.jpg/200/300"
+    photo_url: "https://s-media-cache-ak0.pinimg.com/originals/bf/8a/c8/bf8ac87e3ae2ff56ec7db987edd60d7d.jpg"
   },
   {
     name: "top hat for dogs",
     description: "For the dog who likes nice things",
     stock: 30,
     price: 500,
-    photo_url: "http://2damnfunny.com/wp-content/uploads/2013/12/Classy-Dog-With-a-Fancy-Top-Hat-a-Rich-Mahogany-Smoke-Pipe.png/200/300"
+    photo_url: "http://2damnfunny.com/wp-content/uploads/2013/12/Classy-Dog-With-a-Fancy-Top-Hat-a-Rich-Mahogany-Smoke-Pipe.png"
   },
   {
     name: "pigglet dance outfit",
     description: "For the lil piggies who just wanna dance!",
     stock: 12,
     price: 3050,
-    photo_url: "http://www.fashionworld.co.uk/blog/wp-content/uploads/2016/01/Cute-Animals-Wearing-Clothes-Pigs-in-Tutus-Playing-Pets-25.jpg/200/300"
+    photo_url: "http://www.fashionworld.co.uk/blog/wp-content/uploads/2016/01/Cute-Animals-Wearing-Clothes-Pigs-in-Tutus-Playing-Pets-25.jpg"
   },
   {
     name: "Baby Bunny outfit",
     description: "Are you a new mommy? Dress you little hopper in style!",
     stock: 4,
     price: 1900,
-    photo_url: "http://4.bp.blogspot.com/-hZqBuA0W9W8/Uda9I5QVTiI/AAAAAAAABes/Ijk17zrUo-w/s500/tumblr_lsxp1kA2Ks1r4zdm8o1_500.jpg/200/300"
+    photo_url: "http://4.bp.blogspot.com/-hZqBuA0W9W8/Uda9I5QVTiI/AAAAAAAABes/Ijk17zrUo-w/s500/tumblr_lsxp1kA2Ks1r4zdm8o1_500.jpg"
   }
 ]
 
