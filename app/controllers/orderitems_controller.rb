be9065342d4c38aaa817ad462d24452b40cfc62e
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-19 14:48:28 -0700

    OrderItem model and tests

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,2 +1,38 @@
 class OrderitemsController < ApplicationController
+  def index
+    @order_items = OrderItem.all
+  end
+
+  def new
+    @order_item = OrderItem.new
+  end
+
+  def create
+    @order_item = OrderItem.new(order_params)
+    if @order_item.save
+      redirect_to orderitems_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @order_item = OrderItem.find(params[:id])
+    if @order_item.update(order_item_params)
+      redirect_to orderitems_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    OrderItem.find(params[:id]).destroy
+    redirect_to orderitems_path
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order).permit(:quanity, :product_id, :merchant_id)
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
