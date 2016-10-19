a100a686a6d8133757da923a6b4dc20c78a24222
Yasmin <yasminor@gmail.com>
2016-10-19 11:54:47 -0700

    Generated migration to add uid and provider columns to the merchants table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018220958) do
+ActiveRecord::Schema.define(version: 20161019185033) do
 
   create_table "merchants", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "uid"
+    t.string   "provider"
   end
 
   create_table "order_items", force: :cascade do |t|
