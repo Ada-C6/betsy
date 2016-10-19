3f45994eae7fc95219073157860eb3a33af9591e
Yasmin <yasminor@gmail.com>
2016-10-19 12:25:38 -0700

    Added a class method to the build a Merchant from github credentials

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,14 @@ class Merchant < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   # make email have format requirements
+
+  def self.build_from_github(auth_hash) # factoring method - creates a new User and populates it with data from the auth_hash
+    merchant       = Merchant.new
+    merchant.uid   = auth_hash[:uid]
+    merchant.provider = 'github'
+    merchant.username  = auth_hash['info']['name']
+    merchant.email = auth_hash['info']['email']
+
+    return merchant
+  end
 end
