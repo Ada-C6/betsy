8ce365d521d563dee9e65ab95a7615c7056628f3
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-19 08:46:19 -0700

    wrote Product model and tests

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ActiveRecord::Base
+  has_and_belongs_to_many :categories
+  belongs_to :merchant
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than: 0}
+end
