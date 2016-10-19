04c54dd782b2112b514968948f0d6a142e2f44b9
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-19 14:58:49 -0700

    OrderItem controller test

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class OrderitemsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "will delete an item from orderitems when removed" do
+    orderitem = order_items(:one)
+    assert_difference('OrderItem.count', -1) do
+      delete :destroy, id: orderitem
+    end
+
+    assert_redirected_to orderitems_path
+  end
 end
