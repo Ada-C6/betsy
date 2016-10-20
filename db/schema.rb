d0af4a09f2fe4645cc7fff0a64ce719b8df92077
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-20 15:26:22 -0700

    created (and ran) migration adding total column back into the order table. Yeni was correct! This is needed for cart functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020212452) do
+ActiveRecord::Schema.define(version: 20161020222236) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -47,8 +47,9 @@ ActiveRecord::Schema.define(version: 20161020212452) do
     t.integer  "cc_exp_year"
     t.integer  "cc_exp_month"
     t.integer  "billing_zip"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "total",          default: 0
   end
 
   create_table "products", force: :cascade do |t|
