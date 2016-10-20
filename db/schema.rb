3dd3f40d5f293814dbc52d2c124717eb0ef4cedc
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-20 14:38:00 -0700

    Added category_product join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019185925) do
+ActiveRecord::Schema.define(version: 20161020212610) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_products", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id",  null: false
+  end
+
+  add_index "category_products", ["category_id"], name: "index_category_products_on_category_id"
+  add_index "category_products", ["product_id"], name: "index_category_products_on_product_id"
 
   create_table "merchants", force: :cascade do |t|
     t.string   "username"
@@ -32,6 +46,11 @@ ActiveRecord::Schema.define(version: 20161019185925) do
 
   add_index "order_items", ["order_id"], name: "index_order_items_on_order_id"
   add_index "order_items", ["product_id"], name: "index_order_items_on_product_id"
+
+  create_table "ordered_products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at",     null: false
