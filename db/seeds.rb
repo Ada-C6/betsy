30f4f604f919e9294d648f27592931f2947aed92
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-19 21:29:58 -0700

    Update seed file to read completed csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
 CSV.read('seed_csvs/products.csv').each do |line|
-  product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category: line[3], inventory: line[4] }
+  product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category: line[3], inventory: line[4], description: line[5] }
   Product.create(product_hash)
 end
