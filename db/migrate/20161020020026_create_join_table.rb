e3f4c851597955ee747fc481822f13eeadc22c65
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-19 20:24:45 -0700

    modified join table migration to remove primary key

--- db/migrate/20161020020026_create_join_table.rb
+++ db/migrate/20161020020026_create_join_table.rb
@@ -1,6 +1,6 @@
 class CreateJoinTable < ActiveRecord::Migration
   def change
-    create_join_table :products, :categories do |t|
+    create_join_table :products, :categories, id:false do |t|
       t.index [:product_id, :category_id]
       t.index [:category_id, :product_id]
     end
