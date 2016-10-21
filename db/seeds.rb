4200bd018c77fbc338f6cbf56df6f63acca05ba7
Mary Morrison <marymorrison15@gmail.com>
2016-10-21 12:02:16 -0700

    changed csv category names to category integers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-CSV.read('seed_csvs/products.csv').each do |line|
+CSV.read('db/seed_csvs/products.csv').each do |line|
   product_hash = { name: line[0], price: line[1].to_i, merchant_id: line[2].to_i}
   Product.create(product_hash)
 end
