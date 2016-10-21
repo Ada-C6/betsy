ed74ed826957e36a73970b648efa91bf4f6293d0
heatherherrington <redherring@gmail.com>
2016-10-21 14:31:19 -0700

    Seeds file updated for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,8 @@ CSV.read('db/seed_csvs/products.csv').each do |line|
   product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category: line[3].to_i, inventory: line[4], description: line[5] }
   Product.create(product_hash)
 end
+
+CSV.read('db/seed_csvs/categories.csv').each do |line|
+  category_hash = { name: line[0] }
+  Category.create(category_hash)
+end
