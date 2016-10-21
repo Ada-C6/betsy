be92e98cdaf33bb425572793b0d9308a59752ab2
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-21 12:02:27 -0700

    Update seed to correct path and add category column to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020213543) do
+ActiveRecord::Schema.define(version: 20161021190043) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20161020213543) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "inventory"
+    t.integer  "category"
   end
 
   add_index "products", ["merchant_id"], name: "index_products_on_merchant_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-CSV.read('seed_csvs/all_products.csv').each do |line|
-  product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category: line[3], inventory: line[4], description: line[5] }
+CSV.read('db/seed_csvs/all_products.csv').each do |line|
+  product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category: line[3].to_i, inventory: line[4], description: line[5] }
   Product.create(product_hash)
 end
