2d05fae9c41dfac122407bd36e92e322dbced0e9
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-21 12:12:58 -0700

    working seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021190043) do
+ActiveRecord::Schema.define(version: 20161021190822) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20161021190043) do
     t.datetime "updated_at",  null: false
     t.integer  "inventory"
     t.integer  "category"
+    t.string   "description"
   end
 
   add_index "products", ["merchant_id"], name: "index_products_on_merchant_id"
