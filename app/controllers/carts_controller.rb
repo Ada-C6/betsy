9fdff79a1996c829625253a35966bf18cedf85c5
Shari Meggs <smeggs1@gmail.com>
2016-10-22 18:08:39 -0700

    Fixed indentation

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,32 +1,32 @@
 class CartsController < ApplicationController
-    def index
-      if session[:cart]
-        @cart= session[:cart]
-      else
-        @cart ={}
-      end
+  def index
+    if session[:cart]
+      @cart= session[:cart]
+    else
+      @cart ={}
+    end
+  end
+
+  def add_to_cart
+    id = params[:id]
+
+    if session[:cart]
+      cart = session[:cart]
+    else
+      session[:cart] = {}
+      cart = session[:cart]
     end
 
-    def add_to_cart
-      id = params[:id]
-
-      if session[:cart]
-        cart = session[:cart]
-      else
-        session[:cart] = {}
-        cart = session[:cart]
-      end
-
-      if cart[id]
-        cart[id] = cart[id] + 1
-      else
-        cart[id] = 1
-      end
-      redirect_to carts_path
+    if cart[id]
+      cart[id] = cart[id] + 1
+    else
+      cart[id] = 1
     end
+    redirect_to carts_path
+  end
 
-    def empty_cart
-      session[:cart] = nil
-      redirect_to carts_path
-    end
+  def empty_cart
+    session[:cart] = nil
+    redirect_to carts_path
+  end
 end
