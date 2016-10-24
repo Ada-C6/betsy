942be33e40b2e276f63c3c4a803a4a208a25397e
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-24 12:07:33 -0700

    Move show photo logic to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,8 +8,19 @@ class Product < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :inventory, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :description, presence: true
+  validates :photo_url, presence: true
+
+  def self.find_photo_image(product)
+    if product.photo_url == nil
+      image = "#{ product.name.downcase.gsub(' ', '_') }.jpeg"
+    else
+      image = product.photo_url
+    end
+    return image
+  end
 
   def self.search(search)
-    where(("name LIKE ? OR description LIKE ? OR category LIKE ?"), "%#{search}%", "%#{search}%", "%#{search}%") 
+    where(("name LIKE ? OR description LIKE ? OR category LIKE ?"), "%#{search}%", "%#{search}%", "%#{search}%")
   end
 end
