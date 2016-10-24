54c967e419945b393f845732b981f92a4c3d5f1e
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-24 10:46:28 -0700

    edited review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
-    validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-    validates :description, presence: true, length: { maximum: 400 }
-    validates :product_id, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :description, presence: true, length: { maximum: 400 }
+  validates :product_id, presence: true
 
-    belongs_to :product
+  belongs_to :product
 end
