1d04cb8f3e50a1902180082f26d014b083c7dba1
heatherherrington <redherring@gmail.com>
2016-10-24 14:24:27 -0700

    Adding controller testing

--- test/controllers/guests_controller_test.rb
+++ test/controllers/guests_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class GuestsControllerTest < ActionController::TestCase
-  
-
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 end
