14ccb9c2ac6333163b0e79969cb1e01a75805bf0
heatherherrington <redherring@gmail.com>
2016-10-24 14:13:38 -0700

    Added value to photo_url in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
 CSV.read('db/seed_csvs/products.csv').each do |line|
-  product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category_id: line[3].to_i, inventory: line[4], description: line[5] }
-  Product.create(product_hash)
+  product_hash = { merchant_id: line[0].to_i, name: line[1], price: line[2].to_i, category_id: line[3].to_i, inventory: line[4], description: line[5], photo_url: "1" }
+  Product.create!(product_hash)
 end
 
 CSV.read('db/seed_csvs/categories.csv').each do |line|
   category_hash = { name: line[0] }
-  Category.create(category_hash)
+  Category.create!(category_hash)
 end
