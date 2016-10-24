a81107535acdcae7a80f5ba34f929e6984803706
Yasmin <yasminor@gmail.com>
2016-10-24 11:11:11 -0700

    dropped unnecessary categories_tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024044744) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_tables", force: :cascade do |t|
-  end
+ActiveRecord::Schema.define(version: 20161024180902) do
 
   create_table "category_products", id: false, force: :cascade do |t|
     t.integer "category_id", null: false
