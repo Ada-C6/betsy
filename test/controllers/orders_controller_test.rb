d9002a998f1b8cb288cab21125df290fec04a850
Mary Morrison <marymorrison15@gmail.com>
2016-10-25 14:46:36 -0700

    fixed orders controller tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -55,7 +55,7 @@ class OrdersControllerTest < ActionController::TestCase
     patch :update, id: order, order: { name: "", email: order.email, street_address: order.street_address, city: order.city,
       state: order.state, mailing_zip: order.mailing_zip, cc_number: order.cc_number, cc_exp_month: order.cc_exp_month, cc_exp_year: order.cc_exp_year,
       cc_sec_code: order.cc_sec_code, billing_zip: order.billing_zip }
-      assert_template :edit  
+      assert_template :edit
   end
 
   test "should show the show page for the specified order" do
@@ -80,14 +80,12 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "cannot add invalid order to DB" do
-    post_params = {order: { name: "Igor Smith", street_address: "38 Main St",
-    city: "Seattle", state: "WA", mailing_zip: "98102", cc_number: "1223456743254456", cc_exp_month: 5,
-    cc_exp_year: 2017, cc_sec_code: 328, billing_zip: "98102"} }
-    assert_equal('Order.count', 'Order.count') do
+    assert_difference('Order.count', 0 ) do
+      post_params = {order: { email: "m@gmail.com", street_address: "123 yolo st", city: "Seattle", state: "WA", mailing_zip: "98126", cc_number: "1223456743254456", cc_exp_month: 5, cc_exp_year: 2017, cc_sec_code: 328, billing_zip: 98126 }}
       post :create, post_params
     end
 
-    assert render: :new
+    assert_template :new
   end
 
   test "will delete an order from the DB" do
