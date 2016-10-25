b0d82799b255e721f624fa018340c88c4072754c
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-25 13:43:29 -0700

    wrote home_controller test that ought to pass when require login is disabled before the home index method

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,10 @@
+# INTERNAL_NOTE: this test ought to pass once we make the home index page available to a guest user. 100% simplecov on this controller as of 10.25.2016.
+
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should get index" do
+        get :index
+        assert_response :success
+      end
 end
