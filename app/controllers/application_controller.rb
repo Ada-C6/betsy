35f0bf5c5d24108adb5e0171f6986f24fab08b7a
Yasmin <yasminor@gmail.com>
2016-10-25 15:06:50 -0700

    Added a helper method for current order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if session[:order_id]
+      Order.find(session[:order_id])
+    else
+      Order.create #.create or .new?
+    end
+  end
 
   private
 
