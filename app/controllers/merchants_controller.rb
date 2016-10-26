c230e664d40a4bdbb910a2afaf61a03b4483ce55
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-26 12:41:36 -0700

    merchant controller: created index action. merchant index view: set all of this up with foundation and image link accessibility, mirroring the other product index pages.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,20 +1,24 @@
 class MerchantsController < ApplicationController
 
     # Will likely need to remove this line (or further customize this), once we've narrowed down which pages require login. This line allows our tests to pass.
-    skip_before_action :require_login, only: [:show]
+    skip_before_action :require_login, only: [:index, :show]
 
-  def show
-    begin
-      @merchant = Merchant.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+    def index
+        @merchants = Merchant.all.order(:user_name)
     end
-  end
+
+    def show
+        begin
+            @merchant = Merchant.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+        end
+    end
 
 
-  private
+    private
 
-  def merchant_params
-    params.require(:merchant).permit(:user_name, :email, :uid, :provider)
-  end
+    def merchant_params
+        params.require(:merchant).permit(:user_name, :email, :uid, :provider)
+    end
 end
