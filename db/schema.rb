a697fb67add2b1bcf5ba8478a9ac47e9e38c5e89
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-27 15:06:53 -0700

    remove category_id from product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027021452) do
+ActiveRecord::Schema.define(version: 20161027220418) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 20161027021452) do
     t.integer  "inventory"
     t.string   "description"
     t.string   "photo_url"
-    t.integer  "category_id"
     t.boolean  "active",      default: true
   end
 
