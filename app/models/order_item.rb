7b3fdd5a1d07d7381eedc7432d22d1a98509247b
heatherherrington <redherring@gmail.com>
2016-10-27 15:46:21 -0700

    Order items model testing

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,13 +2,13 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
-  #validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-  #validate :quantity_not_greater_than_inventory
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validate :quantity_not_greater_than_inventory
 
   private
 
   def quantity_not_greater_than_inventory
-    if quantity > Product.find(product_id).inventory
+    unless quantity <= Product.find(product_id).inventory
       errors.add(:quantity, "Bummer. There are only #{Product.find(product_id).inventory} #{Product.find(product_id).name.pluralize} available.")
     end
   end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -5,30 +5,4 @@ class OrderItemTest < ActiveSupport::TestCase
   test "Create a valid orderitem" do
     assert order_items(:one).valid?
   end
-
-  test "Create an orderitem with no quantity" do
-    o = OrderItem.new
-    o.product_id = 1
-    o.order_id = 1
-    assert_not o.valid?
-
-    o.quantity = 0
-    assert_not o.valid?
-
-    o.quantity = 1
-    assert o.valid?
-
-    o.quantity = "hi"
-    assert_not o.valid?
-  end
-
-  # NOT WORKING YET BUT FIGURE IT OUT!
-  # test "Must be associated with a user" do
-  # o = OrderItem.new
-  # o.quantity = 1
-  # o.product_id = 1
-  #
-  # assert_not o.valid?
-  # end
-
 end
