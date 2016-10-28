3a97b00c6f54a4949f5aa4c63d58b733a1a2d270
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-27 21:26:28 -0700

    allow for nil quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -8,11 +8,7 @@ class OrderItem < ActiveRecord::Base
   private
 
   def quantity_not_greater_than_inventory
-    # raise
-    # if quantity == nil
-    #   quantity = 1
-    # end
-    unless quantity <= Product.find(product_id).inventory
+    while quantity == nil || quantity > Product.find(product_id).inventory
       errors.add(:quantity, "Bummer. There are only #{Product.find(product_id).inventory} #{Product.find(product_id).name.pluralize} available.")
     end
   end
